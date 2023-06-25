69e2bbb436851863c0a3c4e5fe118afb6d1d182c
codinglone <codinglone@gmail.com>
2023-06-25 19:55:17 +0400

    Finalized app

--- src/features/todos/ToDoList.jsx
+++ src/features/todos/ToDoList.jsx
@@ -13,7 +13,9 @@ const ToDoList = () => {
     isError,
     error,
     data: todos,
-  } = useQuery("todos", getTodos);
+  } = useQuery("todos", getTodos, {
+    select:  data => data.sort((a, b) => b.id - a.id)
+  });
 
   const addTodoMutation = useMutation(addToDo, {
     onSuccess: () => {
@@ -64,7 +66,26 @@ const ToDoList = () => {
     content =  <p>{error.message}</p>
   }
   else {
-    content = JSON.stringify(todos)
+    content = todos.map((todo) => {
+        return (
+            <article key={todo.id}>
+                <div className="todo">
+                    <input
+                        type="checkbox"
+                        checked={todo.completed}
+                        id={todo.id}
+                        onChange={() =>
+                            updateTodoMutation.mutate({ ...todo, completed: !todo.completed })
+                        }
+                    />
+                    <label htmlFor={todo.id}>{todo.title}</label>
+                </div>
+                <button className="trash" onClick={() => deleteTodoMutation.mutate({ id: todo.id })}>
+                    <FontAwesomeIcon icon={faTrash} />
+                </button>
+            </article>
+        )
+    })
   }
   return ( 
      <main>
